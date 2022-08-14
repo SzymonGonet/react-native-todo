9c0f8004f352a69fe1826c70da06c4e4d21bd6a0
Szymon Gonet <szymovn.gonxt4394@gmail.com>
2022-08-15 00:42:25 +0200

    Update

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,31 +1,32 @@
-import createDataContext from "./createDataContext";
+import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
-    case "delete_blogpost":
-      return state.filter((blogPost) => blogPost.id !== action.payload);
-    case "add_blogpost":
+    case 'delete_blogpost':
+      return state.filter(blogPost => blogPost.id !== action.payload);
+    case 'add_blogpost':
       return [
         ...state,
         {
           id: Math.floor(Math.random() * 99999),
-          title: `Blog Post #${state.length + 1}`,
-        },
+          title: action.payload.title,
+          content: action.payload.content
+        }
       ];
     default:
       return state;
   }
 };
 
-const addBlogPost = (dispatch) => {
-  return () => {
-    dispatch({ type: "add_blogpost" });
+const addBlogPost = dispatch => {
+  return (title, content, callback) => {
+    dispatch({ type: 'add_blogpost', payload: { title, content } });
+    callback();
   };
 };
-
-const deleteBlogPost = (dispatch) => {
-  return (id) => {
-    dispatch({ type: "delete_blogpost", payload: id });
+const deleteBlogPost = dispatch => {
+  return id => {
+    dispatch({ type: 'delete_blogpost', payload: id });
   };
 };
 
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -4,18 +4,16 @@ import {
   Text,
   StyleSheet,
   FlatList,
-  Button,
   TouchableOpacity,
 } from "react-native";
 import { Context } from "../context/BlogContext";
 import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = ({ navigation }) => {
-  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
+  const { state, deleteBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
@@ -29,7 +27,7 @@ const IndexScreen = ({ navigation }) => {
                   {item.title} - {item.id}
                 </Text>
                 <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                  <Feather name="trash" style={styles.icon} />
+                  <Feather style={styles.icon} name="trash" />
                 </TouchableOpacity>
               </View>
             </TouchableOpacity>
@@ -40,14 +38,24 @@ const IndexScreen = ({ navigation }) => {
   );
 };
 
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+        <Feather name="plus" size={30} />
+      </TouchableOpacity>
+    ),
+  };
+};
+
 const styles = StyleSheet.create({
   row: {
     flexDirection: "row",
     justifyContent: "space-between",
-    borderTopWidth: 1,
-    borderColor: "black",
     paddingVertical: 20,
     paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "gray",
   },
   title: {
     fontSize: 18,
